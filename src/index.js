57bf2723ebc254f95c98475c4a79120cb3e6eb53
Steven Espinoza <espisteven5@gmail.com>
2023-11-14 11:49:37 -0800

    refactored index.js and updated css elements

--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
+function el(id) {
+    return document.getElementById(id);
+}
+
+// Constants used for API key and URLs.
 const APIKey = "3db0d220d82f76e1b9db1bcdc4808baf";
 const APIUrlMetirc = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=portland";
 const APIurlImperial = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=";
-const checkWeatherBtn = document.getElementById("searchButton");
-const cityInput = document.getElementById('cityInput');
-const cityName = document.getElementById('weatherLocation');
-const weatherIcon = document.getElementById('weatherIcon');
-const cityTemp = document.getElementById('weatherTemperature');
-const cityHumidity = document.getElementById('weatherHumidity');
-const toggleCelsius = document.getElementById('celsius');
-const toggleFahrenheit = document.getElementById('fahrenheit');
-const commentInput = document.getElementById('comment-input');
-const commentBtn = document.getElementById('submitcomment');
-const commentList = document.getElementById('comments-list')
-const commentForm = document.getElementById('comment-form')
 
+//Global elements used in the app
+const checkWeatherBtn = el("searchButton");
+const cityInput = el('cityInput');
+const cityName = el('weatherLocation');
+const weatherIcon = el('weatherIcon');
+const cityTemp = el('weatherTemperature');
+const cityHumidity = el('weatherHumidity');
+const toggleCelsius = el('celsius');
+const toggleFahrenheit = el('fahrenheit');
+const commentInput = el('comment-input');
+const commentBtn = el('submitcomment');
+const commentList = el('comments-list')
+const commentForm = el('comment-form')
 
+//variable used to store the current weather data
+let currentWeather;
+
+// Event listener for the check weather button 
 checkWeatherBtn.addEventListener('click', () => {
     const city = cityInput.value;
     if (city.trim() !== '') {
@@ -22,46 +32,62 @@ checkWeatherBtn.addEventListener('click', () => {
     }
 })
 
+// Function to fetch weather data for the city
 function fetchWeatherData(city) {
     const APIUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${APIKey}`;
 
     fetch(APIUrl)
         .then(res => res.json())
-        .then(renderCityWeather)
+        .then(weatherData => {
+            currentWeather = weatherData;
+            renderCityWeather(currentWeather);
+        })
         .catch(error => {
             console.error('Error fetching weather data', error);
         });
 }
 
-
+// Fetch initial weather data for Portland. Metric constant above
 fetch(APIUrlMetirc + `&appid=${APIKey}`)
     .then(res => res.json())
     .then(renderCityWeather)
 
+// Function to render the weather data for the city
 function renderCityWeather(weather) {
     cityName.textContent = weather.name;
     const iconURl = `https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`;
     weatherIcon.src = iconURl;
     cityHumidity.textContent = `Humidity: ${weather.main.humidity} %`
 
-    toggleCelsius.addEventListener('change', () => {
-        if (toggleCelsius.checked) {
-            const temperatureCelsius = Math.round(weather.main.temp);
-            cityTemp.textContent = `Temperature: ${temperatureCelsius} °C`;
-        }
-    })
-    toggleFahrenheit.addEventListener('change', () => {
-        if (toggleFahrenheit.checked) {
-            const temperatureFahenheit = Math.round((weather.main.temp * 9 / 5) + 32);
-            cityTemp.textContent = `Temperature: ${temperatureFahenheit} °F`;
-        }
-    });
+    if (toggleCelsius.checked) {
+        const temperatureCelsius = Math.round(weather.main.temp);
+        cityTemp.textContent = `Temperature: ${temperatureCelsius} °C`;
+    }
+    else if (toggleFahrenheit.checked) {
+        const temperatureFahenheit = Math.round((weather.main.temp * 9 / 5) + 32);
+        cityTemp.textContent = `Temperature: ${temperatureFahenheit} °F`;
+    }
 }
 
+// Event listeners for the toggle buttons to switch between Celsius and Fahrenheit
+toggleCelsius.addEventListener('change', () => {
+    if (currentWeather && toggleCelsius.checked) {
+        const temperatureCelsius = Math.round(currentWeather.main.temp);
+        cityTemp.textContent = `Temperature: ${temperatureCelsius} °C`;
+    }
+});
 
+toggleFahrenheit.addEventListener('change', () => {
+    if (currentWeather && toggleFahrenheit.checked) {
+        const temperatureFahrenheit = Math.round((currentWeather.main.temp * 9 / 5) + 32);
+        cityTemp.textContent = `Temperature: ${temperatureFahrenheit} °F`;
+    }
+});
 
+// Event listener for the comment button
 commentForm.addEventListener('submit', handleComments)
 
+// Function to handle the comments
 function handleComments(event) {
     event.preventDefault();
     const commentText = commentInput.value;
@@ -74,7 +100,7 @@ function handleComments(event) {
 
 
 }
-
+// Function to render the comments
 function renderComments(comment) {
     const comments = document.createElement('li');
     comments.textContent = comment;
